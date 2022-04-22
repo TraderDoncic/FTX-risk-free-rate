78ee85f680edc174fc7939f157b82f693d122ccf
Doncic <57617694+Bazluka@users.noreply.github.com>
2022-04-23 09:24:12 +1000

    main

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-from pickle import GET
 import ccxt
-
-ftx = ccxt.ftx()
-
-
-def get_quote(request):
-    markets = ftx.load_markets()
-
-    return{
-        "symbols": list(markets.keys())
-    }
+print("testing")
